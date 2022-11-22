2c4e9ef67ea9b8d738014df071929f60b6aa59dc
gittjar <jarno.keranen@gmail.com>
2022-11-22 13:33:52 +0200

    viimeistelty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  name = "Teksti muuttuu";
-  
+  name = "";
 }
