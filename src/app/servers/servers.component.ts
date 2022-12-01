25fc2e158efb1fbb4c5979cdd891eb82741ab75e
gittjar <jarno.keranen@gmail.com>
2022-12-01 13:11:15 +0200

    backticks template

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // backticks template!
+  template: `
+
+<section>
+<h1>I'm in the Servers Component</h1>
+<h1>NET22S. Jarno</h1>
+<p>Below this I have 2 Server Components. And I'm in Servers Component Template! Backticks template!</p>
+<app-server></app-server>
+<app-server></app-server>
+</section>
+  
+  `
+  ,
+
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
 
+constructor(){}
+
+ngOnInit(): void {
+  
+}
 }
